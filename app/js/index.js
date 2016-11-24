403186156877cb89717fc7275528af6f1c72deff
joriscai <516003559@qq.com>
2016-11-24 16:53:10 +0800

    chang the jquery function to js

--- app/js/index.js
+++ app/js/index.js
@@ -201,17 +201,30 @@
     myChart.setOption(option);
 
     // nav layout
-    flag = 1;
-    nav = $('nav');
-    navPos = $('header').height();
-    $(window).on('scroll', function () {
-        // console.log(navPos);
-        if ($(this).scrollTop() > navPos && flag) {
-            nav.addClass('navFixed');
-            flag = 0;
-        }else if ($(this).scrollTop() < navPos) {
-            nav.removeClass('navFixed');
-            flag = 1;
+    var nav = document.getElementsByTagName('nav')[0];
+    var navPos = document.getElementsByTagName('header')[0].offsetHeight
+                - nav.offsetHeight;
+    var clsName = 'navFixed';
+    // 判断是否有该类选择器
+    function hasClass(obj, cls) {
+        return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+    }
+    document.addEventListener('scroll', function () {
+        var scrollH = this.body.scrollTop;
+        if (scrollH > navPos) {
+            var oClass = nav.className;
+            var blank = (oClass != '') ? ' ' : '';
+            if (!hasClass(nav, clsName)) {
+                nav.className = oClass + blank + clsName;
+                flag = 0;
+            }
+        }else if (scrollH < navPos) {
+            var oClass = nav.className;
+            if (hasClass(nav, clsName)) {
+                var reg = new RegExp('(\\s|^)' + clsName + '(\\s|$)');
+                nav.className = nav.className.replace(reg, '');
+                flag = 1;
+            }
         }
     })
 })();
