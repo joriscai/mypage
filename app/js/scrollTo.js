e3fa3935b98164404ccba840cffa17756a1b92d7
joriscai <516003559@qq.com>
2016-11-27 00:22:45 +0800

    fix IE8 Smooth scroll and preventDefault() Invalid

--- app/js/scrollTo.js
+++ app/js/scrollTo.js
@@ -25,14 +25,20 @@ var addEvent = require('./EventListener');
     if (typeof scroll === 'object') {
         for (var i = 0; i < scroll.length; i++) {
             addEvent(scroll[i], 'click', function (e) {
+                var that = null;
+                // 解决IE8 this指针指向window.event(不是触发的元素)问题
+                // 解决IE8 无法preventDefault()方法禁止默认行为
                 try {
                     e.preventDefault();
+                    that = this;
                 } catch (e) {
+                    window.event.returnValue = false;
+                    that = window.event.srcElement;
                 }
 
-                var name = this.getAttribute('scroll');
-                var that = document.getElementById(name)
-                var pos = that.offsetTop - that.style.padding;
+                var name = that.getAttribute('scroll');
+                var target = document.getElementById(name)
+                var pos = target.offsetTop - target.style.padding;
                 var nav = document.getElementsByTagName('nav')[0].offsetHeight;
 
                 smoothScroll(pos, nav)
